17df74587e43ff65bd18c47be494b86ccc446999
motorpsico97 <motorpsico1917@gmail.com>
2026-02-06 01:10:41 -0300

    db

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.URI_MONGODB);     
+        await mongoose.connect(process.env.MONGO_URI);     
         console.log("Conectado a MongoDB");   
     } catch (error) {
         console.error("Error al conectar a MongoDB:", error);
